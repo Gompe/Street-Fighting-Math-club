63c8d0d4ea3515dbd1c0ba13d4733328e5556c1d
Pedro Cabral <pedro.cabral@polytechnique.edu>
2022-02-22 22:38:51 +0100

    first commit

--- sfmc-frontend/src/utils/alertTimed.js
+++
@@ -1,6 +0,0 @@
-const alertTimed = (alertDispatch, action) => {
-  alertDispatch(action)
-  setTimeout(5000, () => alertDispatch('CLOSE_ALERT'))
-}
-
-export default alertTimed
--- sfmc-frontend/src/views/QuestionView.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-const QuestionView = () => {
-  const { questionId } = useParams()
-}
